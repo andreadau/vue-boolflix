18d70e611be18e467c9d0aaecf337bf2d984e96f
Andrea <dauriaandrea99@gmail.com>
2021-01-13 11:43:34 +0100

    Serie Tv Add

--- Milestone 1/assets/js/app.js
+++ Milestone 1/assets/js/app.js
@@ -2,6 +2,7 @@ let vm = new Vue({
     el: "#app",
     data: {
         films: [],
+        series: [],
         search: ""
     },
     methods: {
@@ -15,7 +16,21 @@ let vm = new Vue({
             })
             .then(response =>{
                 this.films = response.data.results;
-                console.log(this.films);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        },
+        seriesCall(){
+            axios.get("https://api.themoviedb.org/3/search/tv", {
+                params: {
+                    api_key: "3bc4b75887deae6d9b5830490b66e695",
+                    language: "it-IT",
+                    query: this.search
+                }
+            })
+            .then(response =>{
+                this.series = response.data.results;
             })
             .catch(error => {
                 console.log(error);
@@ -24,5 +39,6 @@ let vm = new Vue({
     },
     mounted(){
         this.filmCall();
+        this.seriesCall();
     },
 });
