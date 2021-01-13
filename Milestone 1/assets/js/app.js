5ed871b205e05dfdb4a68337d7551fdcd913d39c
Andrea <dauriaandrea99@gmail.com>
2021-01-13 12:17:09 +0100

    Milestone 2 !END!

--- Milestone 1/assets/js/app.js
+++ Milestone 1/assets/js/app.js
@@ -2,7 +2,6 @@ let vm = new Vue({
     el: "#app",
     data: {
         films: [],
-        series: [],
         search: ""
     },
     methods: {
@@ -16,36 +15,14 @@ let vm = new Vue({
             })
             .then(response =>{
                 this.films = response.data.results;
+                console.log(this.films);
             })
             .catch(error => {
                 console.log(error);
             });
         },
-        seriesCall(){
-            axios.get("https://api.themoviedb.org/3/search/tv", {
-                params: {
-                    api_key: "3bc4b75887deae6d9b5830490b66e695",
-                    language: "it-IT",
-                    query: this.search
-                }
-            })
-            .then(response =>{
-                this.series = response.data.results;
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-        starGenerator(vote,index){
-            if (Math.ceil(vote) / 2 > index){
-                return "fas fa-star"
-            } else {
-                return "far fa-star"
-            }
-        }
     },
     mounted(){
         this.filmCall();
-        this.seriesCall();
     },
 });
